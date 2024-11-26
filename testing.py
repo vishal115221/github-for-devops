89d26cf71c1a032ac7f7530df1ad88458f22d1fa
vishal <vishabharadwaj@gmail.com>
2024-11-26 21:12:11 +0530

    ghj

--- testing.py
+++ testing.py
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
-print("Hello dosto, lets learn git")
-=======
+
 print("Hello dosto, lets learn git")
 print("commit through git")
 print("lets go")
->>>>>>> b2214fdb04b45b6a755f17d8432f1b6c31b1c266
+
